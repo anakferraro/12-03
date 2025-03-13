24ca1c773db5230109520a925c6dcc87b38c706c
anakferraro <ferraroanakarolina@gmail.com>
2025-03-12 23:41:04 -0300

    Add files via upload

---
+++ exercicio_while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void) {
+    int contador= 1;
+    
+    while (contador <= 10 ) {
+    printf("%d  ", contador);
+    
+    contador = contador + 1;
+} 
+
+  return 0;
+   
+    
+}
